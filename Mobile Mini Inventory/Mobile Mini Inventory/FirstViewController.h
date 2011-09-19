a7c8d44096b2f3636e9071f89eb1e21347f555b5
Dave Hilton <davejhilton1@gmail.com>
2011-09-19 10:37:53 -0600

    added Readme

---
+++ Mobile Mini Inventory/Mobile Mini Inventory/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  Mobile Mini Inventory
+//
+//  Created by Dave Hilton on 9/18/11.
+//  Copyright 2011 Dave Hilton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ Mobile Mini Inventory/Mobile Mini Inventory/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Mobile Mini Inventory
+//
+//  Created by Dave Hilton on 9/18/11.
+//  Copyright 2011 Dave Hilton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	int retVal = UIApplicationMain(argc, argv, nil, nil);
+	[pool release];
+	return retVal;
+}
